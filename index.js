126b6986d75b854754baf9d55a75ed6be27fbfcb
Innocenzo <innocenzo@MacBook-Pro-di-Innocenzo.local>
2023-05-25 23:51:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+
+const configuration = new Configuration({
+    organization: "org-kRaOiaGO8eEClgIQaQwqWPmr",
+    apiKey: "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"
+});
+
+const openai = new OpenAIApi(configuration);
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", async(req, res) => {
+    const completion = await openai.createChatCompletion({
+        model : "gpt-3.5-turbo",
+        messages: [
+            {role: "user", content: "Chi ha costruito la statua del Nettuno di Bologna"}
+        ]
+    })
+
+    res.json(completion.data.choices[0].message);
+});
+
+app.listen(port, () => {
+    console.info('openai app listen on port 3000');
+})
